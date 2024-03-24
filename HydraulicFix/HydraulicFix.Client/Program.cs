ad1f42fd54ef19b719b0ca89d836b059682c6ca5
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-03-24 03:29:13 -0400

    Se agregó Radzen Notifications

--- HydraulicFix/HydraulicFix.Client/Program.cs
+++ HydraulicFix/HydraulicFix.Client/Program.cs
@@ -4,6 +4,7 @@ using HydraulicFix.Client.Service;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Radzen;
 using Shared.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,9 +12,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
 builder.Services.AddScoped<IHydraulicAsp<IdentityUserRole<string>>, UserRolesService>();
 builder.Services.AddScoped<IHydraulicAsp<IdentityRole>, RolesService>();
 builder.Services.AddScoped<IHydraulicAsp<ApplicationUserDto>, UsersService>();
+builder.Services.AddScoped<NotificationService>();
 
 builder.Services.AddScoped(http => new HttpClient
 {
